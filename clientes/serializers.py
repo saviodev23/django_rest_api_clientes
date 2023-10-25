971364f62793364297549d04dbf2b87262ccf4cc
Saviodev23 <saviodev21@gmail.com>
2023-10-25 19:28:32 -0300

    finalizando API

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from clientes.models import Cliente
-
+from clientes.validators import *
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"Número de CPF inválido!"})   
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': "Não inclua números neste campo."})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': "O RG deve ter 9 digitos."})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O número de celular deve seguir esse padrão: 77 91234-1234."})
+        return data
     
+    #A função sempre deve começar com "validade_"
+    # def valida_cpf(self, cpf):
+    #     if not len(cpf) != 11:
+    #         raise serializers.ValidationError('CPF deve conter pelo menos 11 digitos')
--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets
+
+from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
-
+from django_filters.rest_framework import DjangoFilterBackend
 class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]#faz com que seja possivel fazer o filtro dos clientes
+    ordering_fields = ['nome']#filtrando os clientes por nome
+    search_fields = ['nome', 'cpf']#possibilita fazer a busca por nome e cpf do cliente
+    filterset_fields = ['ativo']#busca exata, verdadeiro ou falso
